63dbc4b796dca31f2b651e7a7ac6de4edf892887
Weeravaradhana <thaminduweeravardana@gmail.com>
2025-07-19 20:36:02 +0530

    Initial commit

---
+++ src/main/java/com/devstack/whilecart/whilecart_gateway/WhilecartGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.devstack.whilecart.whilecart_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhilecartGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhilecartGatewayApplication.class, args);
+	}
+
+}
